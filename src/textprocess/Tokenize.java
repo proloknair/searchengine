528f0d5caaf8ab3942b11dd1e19513f3a77c82e7
prolok1990 <prolok1990@gmail.com>
2015-01-15 23:59:30 -0800

    Init2

---
+++ src/textprocess/Tokenize.java
@@ -0,0 +1,19 @@
+package textprocess;
+import java.util.*;
+import java.io.*;
+
+public class Tokenize {
+	List<String> tokenizeFile(String file)
+	{
+		ArrayList<String> tokens=new ArrayList<String>();
+		
+		return tokens;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
