759f69d5c16b3dea742a9db185f380309e749e92
prolok1990 <prolok1990@gmail.com>
2015-01-16 01:25:47 -0800

    Tokenize

--- src/textprocess/Tokenize.java
+++ src/textprocess/Tokenize.java
@@ -1,18 +1,54 @@
 package textprocess;
 import java.util.*;
 import java.io.*;
+import java.util.regex.*;
 
 public class Tokenize {
-	List<String> tokenizeFile(String file)
+	List<String> tokenizeFile(String TextFile) throws IOException
 	{
-		ArrayList<String> tokens=new ArrayList<String>();
+		List<String> tokenslist=new ArrayList<String>();
+		BufferedReader br=null;
+		try{
+			
+			br=new BufferedReader(new InputStreamReader(new FileInputStream(TextFile),"US-ASCII"));
+			String line;
+			
+			while((line=br.readLine())!=null)
+			{
+				Matcher m = Pattern.compile("\\w+").matcher(line);
+				while(m.find())
+				{
+					tokenslist.add(m.group());
+				}
+			}
+			
+			
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return tokenslist;
+	}
+	void print(List<String> tokens )
+	{
+		for(String str:tokens)
+		{
+			System.out.println(str);
+		}
 		
-		return tokens;
 	}
 	
-	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		Tokenize tok = new Tokenize();
+		List<String> tokens=tok.tokenizeFile("/Users/proloknair/Dropbox/IR-Proj/SearchEngine/Text.txt");
+		tok.print(tokens);
 
 	}
 
