3e77dddac78359cf268ce797f91df365f8ed341c
prolok1990 <prolok1990@gmail.com>
2015-01-16 16:22:44 -0800

    Word frequencies and printing in descending order

--- src/textprocess/Tokenize.java
+++ src/textprocess/Tokenize.java
@@ -1,7 +1,22 @@
 package textprocess;
-import java.util.*;
-import java.io.*;
-import java.util.regex.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+
 
 public class Tokenize {
 	List<String> tokenizeFile(String TextFile) throws IOException
@@ -36,19 +51,59 @@ public class Tokenize {
 		}
 		return tokenslist;
 	}
-	void print(List<String> tokens )
+	Map<String,Integer> computeWordFrequencies(List<String> tokenlist)
 	{
-		for(String str:tokens)
+		Map<String,Integer> freqcount=new HashMap<String,Integer>();
+		for(String str:tokenlist)
+		{
+			if(freqcount.containsKey(str))
+			{
+				freqcount.put(str,freqcount.get(str)+1);
+			}
+			else
+			{
+				freqcount.put(str, 1);
+			}
+		}
+		 
+		 
+			    
+		return freqcount;
+	}
+	void print(List<String> tokenlist)
+	{
+		for(String str:tokenlist)
 		{
 			System.out.println(str);
 		}
+		
+	}
+	void print(Map<String,Integer> fcount)
+	{
+		List<Map.Entry<String,Integer>> list = new LinkedList<>( fcount.entrySet() );
+	    Collections.sort( list, new Comparator<Map.Entry<String,Integer>>()
+	    {
+	        @Override
+	        public int compare( Map.Entry<String,Integer> m1, Map.Entry<String,Integer> m2 )
+	        {
+	            return (m2.getValue()).compareTo( m1.getValue() );
+	        }
+
+		} );
+
+	    
+	    for (Map.Entry<String,Integer> entry : list)
+	    {
+	    	System.out.println( entry.getKey()+" : "+entry.getValue());
+	    }
 		
 	}
 	
 	public static void main(String[] args) throws IOException {
 		Tokenize tok = new Tokenize();
 		List<String> tokens=tok.tokenizeFile("/Users/proloknair/Dropbox/IR-Proj/SearchEngine/Text.txt");
-		tok.print(tokens);
+		Map<String,Integer> fcount=tok.computeWordFrequencies(tokens);
+		tok.print(fcount);
 
 	}
 
